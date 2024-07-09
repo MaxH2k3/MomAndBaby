64151fcfc3f334575c215c7b7cccb894da03d3e1
Lê Huy <huy110903@gmail.com>
2024-07-09 22:27:18 +0700

    update dashboard

--- MomAndBaby/Utilities/Constants/ConstantPage.cs
+++ MomAndBaby/Utilities/Constants/ConstantPage.cs
@@ -29,13 +29,7 @@
             public const string Header = PrePathComponentDashboard + "/Common/Header";
             public const string Footer = PrePathComponentDashboard + "/Common/Footer";
 
-            public const string SideHeader = PrePathComponentDashboard + "/Elements/SideHeader";
-            // public const string SidebarMenu = PrePathComponentDashboard + "/Elements/SidebarMenu";
-            // public const string SideMenu = PrePathComponentDashboard + "/Elements/SideMenu";
-
-            // public const string QuickView = "QuickView";
-            //
-            // public const string Chat = "../Chat/Menu";
+            public const string Menu = PrePathComponentDashboard + "/Common/Menu";
         }
     }
 }
