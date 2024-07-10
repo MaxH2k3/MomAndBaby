4022a3fa03fb32e6d630d68d9526c6fb65cb71b8
Lê Huy <huy110903@gmail.com>
2024-07-10 10:27:22 +0700

    update description

---
+++ MomAndBaby/wwwroot/quill-editor/editor.js
@@ -0,0 +1,39 @@
+﻿document.addEventListener("DOMContentLoaded", () => {
+    let quill = new Quill('#editor', options);
+
+    quill.on('text-change', () => {
+        let content = quill.root.innerHTML;
+        document.getElementById("editor-input").value = content;
+    });
+    
+});
+
+let toolbarOptions = [
+    ['bold', 'italic', 'underline', 'strike'],        // toggled buttons
+    ['blockquote', 'code-block'],
+
+    [{ 'header': 1 }, { 'header': 2 }],               // custom button values
+    [{ 'list': 'ordered'}, { 'list': 'bullet' }],
+    [{ 'script': 'sub'}, { 'script': 'super' }],      // superscript/subscript
+    [{ 'indent': '-1'}, { 'indent': '+1' }],          // outdent/indent
+    [{ 'direction': 'rtl' }],                         // text direction
+
+    [{ 'size': ['small', false, 'large', 'huge'] }],  // custom dropdown
+    [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
+
+    [{ 'color': [] }, { 'background': [] }],          // dropdown with defaults from theme
+    [{ 'font': [] }],
+    [{ 'align': [] }],
+
+    ['clean'],                                         // remove formatting button
+    
+    ['link']
+];
+
+let options = {
+    modules: {
+        toolbar: toolbarOptions,
+    },
+    theme: 'snow',
+    placeholder: 'description...'
+};
