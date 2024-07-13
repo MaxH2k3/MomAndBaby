b58ab10fd4cdd5c8c25db066cb83aca3a39a87b8
thanhnguyen <look1692003@gmail.com>
2024-07-13 17:13:11 +0700

    fix bug orderdetail

--- MomAndBaby/Pages/Main/Body/CartDetail.cshtml.cs
+++ MomAndBaby/Pages/Main/Body/CartDetail.cshtml.cs
@@ -40,11 +40,11 @@ public class CartDetailModel : PageModel
 
     public void OnGet()
     {
-        if (!User.Identity!.IsAuthenticated)
-        {
-            Redirect("/login");
-        }
-
+        if(!User.Identity!.IsAuthenticated)
+            {
+                Redirect("/login");
+            }
+        
         var cart = HttpContext.Session.GetString("Cart");
         if (!string.IsNullOrEmpty(cart))
         {
@@ -99,9 +99,9 @@ public class CartDetailModel : PageModel
     public async Task<IActionResult> OnPostPay()
     {
         if (!ModelState.IsValid)
-        {
-            return Page();
-        }
+            {
+                return Page();
+            }
         HttpContext.Session.SetString("Address", JsonConvert.SerializeObject(Address));
         var sessionData = HttpContext.Session.GetString("Total");
         if (JsonConvert.DeserializeObject<Decimal>(sessionData) == 0)
@@ -124,7 +124,7 @@ public class CartDetailModel : PageModel
             // Save the order details to the database
             await SaveOrderDetails();
             // Clear the cart after successful payment
-            HttpContext.Session.Remove("Cart");
+             HttpContext.Session.Remove("Cart");
             HttpContext.Session.Remove("Total");
 
             return RedirectToPage("/cart-detail");
@@ -136,6 +136,8 @@ public class CartDetailModel : PageModel
 
     private async Task SaveOrderDetails()
     {
+        var cart = HttpContext.Session.GetString("Cart");
+         var cartData = JsonConvert.DeserializeObject<List<CartSessionModel>>(cart);
         var sessionData = HttpContext.Session.GetString("Total");
         // var firstNameData = HttpContext.Session.GetString("FirstName");
         // var lastNameData = HttpContext.Session.GetString("LastName");
@@ -157,11 +159,11 @@ public class CartDetailModel : PageModel
 
         };
         var orderSave = await _orderService.CreateOrder(order);
-        var orderDetails = CartItems.Select(cartItem => new MomAndBaby.BusinessObject.Entity.OrderDetail
+        var orderDetails = cartData.Select(cartItem => new MomAndBaby.BusinessObject.Entity.OrderDetail
         {
             OrderId = orderSave,
             ProductId = cartItem.Id,
-            Quantity = 1,
+            Quantity = 1, 
             Price = cartItem.UnitPrice.Value
         }).ToList();
 
