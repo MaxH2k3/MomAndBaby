2507bb931b781ccd9b1a9251079f17312e4396e7
Lê Huy <huy110903@gmail.com>
2024-07-18 13:28:27 +0700

    fix connection for notification

--- MomAndBaby/Subscribe/SubscribeNotification.cs
+++ MomAndBaby/Subscribe/SubscribeNotification.cs
@@ -16,7 +16,7 @@ namespace MomAndBaby.Subscribe
 
         public void SubscribeTableDependency()
         {
-            _tableDependency = new SqlTableDependency<Notification>("server=35.240.220.220,1433;database=MomAndBaby;uid=sa;pwd=Admin12345@;TrustServerCertificate=true;Encrypt=True;Connection Timeout=30;");
+            _tableDependency = new SqlTableDependency<Notification>("server=35.240.220.220,1433;database=MomAndBaby;uid=sa;pwd=Admin12345@;TrustServerCertificate=true;Encrypt=True;");
             _tableDependency.OnChanged += TableDependency_OnChanged;
             _tableDependency.OnError += TableDependency_OnError;
             _tableDependency.Start();
